423218a8611bb16a966637fa6c84ff7f4501bc04
Liam Cohen <licohen@made-f0Br6s5.clusters.42paris.fr>
2025-04-10 16:38:35 +0200

    ajout commentaire

--- includes/struct_cub.h
+++ includes/struct_cub.h
@@ -6,7 +6,7 @@
 /*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:57:47 by licohen           #+#    #+#             */
-/*   Updated: 2025/04/10 15:58:42 by licohen          ###   ########.fr       */
+/*   Updated: 2025/04/10 16:38:05 by licohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 
 enum e_state
 {
-	ERROR = -1,
-	FALSE,
-	TRUE
+    ERROR = -1,
+    FALSE,
+    TRUE
 };
 
-typedef struct	s_test_map_node
+typedef struct s_test_map_node
 {
-	char					*line;
-	struct s_test_map_node	*next;
-}	t_test_map_node;
+    char *line;
+    struct s_test_map_node *next;
+} t_test_map_node;
 
 /* Structures basiques */
 typedef struct s_dims
@@ -47,7 +47,7 @@ typedef struct s_color
     unsigned char r;
     unsigned char g;
     unsigned char b;
-    int value;      // Valeur combinée pour un accès rapide
+    int value;
 } t_color;
 
 /* Structures liées aux images et textures */
@@ -62,9 +62,9 @@ typedef struct s_img
 
 typedef struct s_texture
 {
-    char *path;           // Chemin vers le fichier de texture
-    void *img;            // Pointeur vers l'image MLX
-    int *addr;            // Adresse des données de l'image
+    char *path;
+    void *img;
+    int *addr;
     int width;
     int height;
     int bits_per_pixel;
@@ -75,35 +75,35 @@ typedef struct s_texture
 /* Structure du joueur */
 typedef struct s_player
 {
-    double x;             // Position X dans la grille
-    double y;             // Position Y dans la grille
-    double pos_x;         // Position X pour le rendu
-    double pos_y;         // Position Y pour le rendu
-    double dir_x;         // Direction X (vecteur de direction)
-    double dir_y;         // Direction Y (vecteur de direction)
-    double plane_x;       // Plan de caméra X (vecteur perpendiculaire)
-    double plane_y;       // Plan de caméra Y (vecteur perpendiculaire)
-    double move_speed;    // Vitesse de déplacement
-    double rot_speed;     // Vitesse de rotation
-    int move_forward;     // État du mouvement vers l'avant
-    int move_backward;    // État du mouvement vers l'arrière
-    int move_left;        // État du mouvement latéral gauche
-    int move_right;       // État du mouvement latéral droit
-    int rotate_left;      // État de la rotation gauche
-    int rotate_right;     // État de la rotation droite
-    int rotation_direction; // Direction de rotation (-1, 0, 1)
+    double x;
+    double y;
+    double pos_x;
+    double pos_y;
+    double dir_x;
+    double dir_y;
+    double plane_x;
+    double plane_y;
+    double move_speed;
+    double rot_speed;
+    int move_forward;
+    int move_backward;
+    int move_left;
+    int move_right;
+    int rotate_left;
+    int rotate_right;
+    int rotation_direction;
 } t_player;
 
 /* Structure de la minimap */
 typedef struct s_minimap
 {
-    int scale;            // Échelle de la minimap par rapport à la grille
-    int width;            // Largeur en pixels
-    int height;           // Hauteur en pixels
-    int pos_x;            // Position X dans la fenêtre
-    int pos_y;            // Position Y dans la fenêtre
-    void *img;            // Pointeur vers l'image MLX
-    int *addr;            // Adresse des données de l'image
+    int scale;
+    int width;
+    int height;
+    int pos_x;
+    int pos_y;
+    void *img;
+    int *addr;
     int bits_per_pixel;
     int line_length;
     int endian;
@@ -112,160 +112,160 @@ typedef struct s_minimap
 /* Structures liées au raycasting */
 typedef struct s_ray
 {
-    double ray_angle;     // Angle du rayon
-    double distance;      // Distance au mur
-    int wall_hit_x;       // Position X du point d'impact
-    int wall_hit_y;       // Position Y du point d'impact
-    int is_vertical;      // Si le mur touché est vertical
-    int facing_up;        // Si le rayon pointe vers le haut
-    int facing_right;     // Si le rayon pointe vers la droite
+    double ray_angle;
+    double distance;
+    int wall_hit_x;
+    int wall_hit_y;
+    int is_vertical;
+    int facing_up;
+    int facing_right;
 } t_ray;
 
 typedef struct s_wall_render
 {
-    int wall_height;      // Hauteur du mur à l'écran
-    int wall_top;         // Coordonnée Y du haut du mur
-    int wall_bottom;      // Coordonnée Y du bas du mur
-    double wall_x;        // Position X exacte du point d'impact (pour texture)
-    int tex_x;            // Coordonnée X dans la texture
-    double step;          // Pas de texture par pixel
-    double tex_pos;       // Position actuelle dans la texture
-    double shade;         // Facteur d'ombrage (distance)
-    int tex_y;            // Coordonnée Y dans la texture
-    t_player *player;     // Référence au joueur
+    int wall_height;
+    int wall_top;
+    int wall_bottom;
+    double wall_x;
+    int tex_x;
+    double step;
+    double tex_pos;
+    double shade;
+    int tex_y;
+    t_player *player;
 } t_wall_render;
 
 typedef struct s_color_data
 {
-    int ceiling_color;    // Couleur du plafond
-    int floor_color;      // Couleur du sol
-    unsigned int color;   // Couleur de travail
-    unsigned char r;      // Composante rouge
-    unsigned char g;      // Composante verte
-    unsigned char b;      // Composante bleue
+    int ceiling_color;
+    int floor_color;
+    unsigned int color;
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
 } t_color_data;
 
 typedef struct s_dda_data
 {
-    double ray_dir_x;     // Direction X du rayon
-    double ray_dir_y;     // Direction Y du rayon
-    double delta_dist_x;  // Distance entre intersections verticales
-    double delta_dist_y;  // Distance entre intersections horizontales
-    int map_x;            // Position X dans la grille
-    int map_y;            // Position Y dans la grille
-    double side_dist_x;   // Distance à la prochaine intersection verticale
-    double side_dist_y;   // Distance à la prochaine intersection horizontale
-    int step_x;           // Direction de pas en X (-1, 0, 1)
-    int step_y;           // Direction de pas en Y (-1, 0, 1)
+    double ray_dir_x;
+    double ray_dir_y;
+    double delta_dist_x;
+    double delta_dist_y;
+    int map_x;
+    int map_y;
+    double side_dist_x;
+    double side_dist_y;
+    int step_x;
+    int step_y;
 } t_dda_data;
 
 /* Structures liées au rendu */
 typedef struct s_renderer
 {
-    void *mlx;            // Pointeur MLX
-    void *win;            // Pointeur vers la fenêtre
-    t_img frame;          // Image principale pour le rendu
-    t_minimap minimap;    // Minimap
-    t_texture *current_texture; // Texture actuelle en cours d'utilisation
-    int render_flags;     // Drapeaux pour les options de rendu
+    void *mlx;
+    void *win;
+    t_img frame;
+    t_minimap minimap;
+    t_texture *current_texture;
+    int render_flags;
 } t_renderer;
 
 /* Structures de gestion des états du jeu */
 typedef struct s_game_state
 {
-    int is_running;       // Si le jeu est en cours d'exécution
-    int window_focused;   // Si la fenêtre a le focus
-    int mouse_captured;   // Si la souris est capturée
-    double last_frame;    // Temps du dernier frame
-    double delta_time;    // Temps entre les frames
-    double fps;           // Frames par seconde
-    int status_anim_frame; // Frame d'animation pour les statuts
-    int status_transitioning; // Si une transition est en cours
+    int is_running;
+    int window_focused;
+    int mouse_captured;
+    double last_frame;
+    double delta_time;
+    double fps;
+    int status_anim_frame;
+    int status_transitioning;
 } t_game_state;
 
 typedef struct s_input
 {
-    int move_forward;     // Touche W
-    int move_backward;    // Touche S
-    int move_left;        // Touche A
-    int move_right;       // Touche D
-    int rotate_left;      // Flèche gauche
-    int rotate_right;     // Flèche droite
-    int last_mouse_x;     // Dernière position X de la souris
-    int last_mouse_y;     // Dernière position Y de la souris
-    double mouse_sensitivity; // Sensibilité de la souris
+    int move_forward;
+    int move_backward;
+    int move_left;
+    int move_right;
+    int rotate_left;
+    int rotate_right;
+    int last_mouse_x;
+    int last_mouse_y;
+    double mouse_sensitivity;
 } t_input;
 
 /* Structure de configuration de la carte (existante) */
 typedef struct s_map_config
 {
-    char *no_path;        // Chemin de la texture Nord
-    char *so_path;        // Chemin de la texture Sud
-    char *we_path;        // Chemin de la texture Ouest
-    char *ea_path;        // Chemin de la texture Est
-    int f_color[3];       // Couleur du sol (RGB)
-    int c_color[3];       // Couleur du plafond (RGB)
-    char **map;           // Grille de la carte
-    struct s_test_map_node *test_map;  // Liste chaînée temporaire pour parser la carte
-    int map_height;       // Hauteur de la carte
-    int map_width;        // Largeur de la carte
-    char start_dir;       // Direction de départ du joueur (N, S, E, W)
-    int start_y;          // Position Y de départ du joueur
-    int start_x;          // Position X de départ du joueur
+    char *no_path;
+    char *so_path;
+    char *we_path;
+    char *ea_path;
+    int f_color[3];
+    int c_color[3];
+    char **map;
+    struct s_test_map_node *test_map;
+    int map_height;
+    int map_width;
+    char start_dir;
+    int start_y;
+    int start_x;
 } t_map_config;
 
 /* Structure des données de la carte */
 typedef struct s_map_data
 {
-    char **grid;          // Grille de la carte (0 = vide, 1 = mur, etc.)
-    int width;            // Largeur de la grille
-    int height;           // Hauteur de la grille
-    char **flood_grid;    // Grille utilisée pour la vérification par inondation
-    t_color floor;        // Couleur du sol
-    t_color ceiling;      // Couleur du plafond
-    t_texture north;      // Texture pour les murs orientés nord
-    t_texture south;      // Texture pour les murs orientés sud
-    t_texture east;       // Texture pour les murs orientés est
-    t_texture west;       // Texture pour les murs orientés ouest
-    char player_start_dir; // Direction initiale du joueur
-    double player_start_x; // Position X initiale du joueur
-    double player_start_y; // Position Y initiale du joueur
-    char player_dir;      // Direction actuelle du joueur
-    double player_x;      // Position X actuelle du joueur
-    double player_y;      // Position Y actuelle du joueur
-    t_map_config *map_config; // Pointeur vers la configuration originale
+    char **grid;
+    int width;
+    int height;
+    char **flood_grid;
+    t_color floor;
+    t_color ceiling;
+    t_texture north;
+    t_texture south;
+    t_texture east;
+    t_texture west;
+    char player_start_dir;
+    double player_start_x;
+    double player_start_y;
+    char player_dir;
+    double player_x;
+    double player_y;
+    t_map_config *map_config;
 } t_map_data;
 
 /* Structure principale du jeu */
 typedef struct s_game
 {
-    t_renderer renderer;  // Gestionnaire de rendu
-    t_game_state state;   // État du jeu
-    t_input input;        // Gestion des entrées
-    t_map_data *map_data; // Données de la carte
-    t_player player;      // Données du joueur
-    char **map;           // Référence à la grille pour un accès rapide
-    double move_speed;    // Vitesse de déplacement (peut être ajustée)
+    t_renderer renderer;
+    t_game_state state;
+    t_input input;
+    t_map_data *map_data;
+    t_player player;
+    char **map;
+    double move_speed;
 } t_game;
 
 /* Structures auxiliaires pour le dessin */
 typedef struct s_square_data
 {
-    int scale;            // Échelle du carré
-    int offset_x;         // Décalage X
-    int offset_y;         // Décalage Y
-    int map_x;            // Position X dans la grille
-    int map_y;            // Position Y dans la grille
-    int color;            // Couleur du carré
+    int scale;
+    int offset_x;
+    int offset_y;
+    int map_x;
+    int map_y;
+    int color;
 } t_square_data;
 
 typedef struct s_draw_player_data
 {
-    int size;             // Taille du marqueur du joueur
-    double angle;         // Angle de rotation
-    int arrow_x[3];       // Coordonnées X pour dessiner une flèche
-    int arrow_y[3];       // Coordonnées Y pour dessiner une flèche
-    t_player *player;     // Référence au joueur
+    int size;
+    double angle;
+    int arrow_x[3];
+    int arrow_y[3];
+    t_player *player;
 } t_draw_player_data;
 
 #endif
