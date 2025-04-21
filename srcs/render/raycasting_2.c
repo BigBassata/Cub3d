80823bd362f771e6a409f86aca6f8104203ff8a6
julienLombard <julienlombard.fr@gmail.com>
2025-04-21 18:24:48 +0200

    refacto raycasting_2.c

--- srcs/render/raycasting_2.c
+++ srcs/render/raycasting_2.c
@@ -14,37 +14,57 @@
 
 void	compute_wall_slice(t_game *game)
 {
-	game->col_render.line_height = (int)(game->renderer.win_height / game->col_render.perp_wall_dist);
-	game->col_render.draw_start = -game->col_render.line_height / 2 + game->renderer.win_height / 2;
+	int	h;
+
+	h = game->renderer.win_height;
+	game->col_render.line_height = (int)(h / game->col_render.perp_wall_dist);
+	game->col_render.draw_start = -game->col_render.line_height / 2 + h / 2;
 	if (game->col_render.draw_start < 0)
 		game->col_render.draw_start = 0;
-	game->col_render.draw_end = game->col_render.line_height / 2 + game->renderer.win_height / 2;
-	if (game->col_render.draw_end >= game->renderer.win_height)
-		game->col_render.draw_end = game->renderer.win_height - 1;
+	game->col_render.draw_end = game->col_render.line_height / 2 + h / 2;
+	if (game->col_render.draw_end >= h)
+		game->col_render.draw_end = h - 1;
 }
 
 void	compute_wall_hit_position(t_game *game)
 {
-	game->col_render.wall_x = (game->dda.side == 0)
-		? game->player.pos_y + game->col_render.perp_wall_dist * game->dda.ray_dir_y
-		: game->player.pos_x + game->col_render.perp_wall_dist * game->dda.ray_dir_x;
+	double	d;
+
+	d = game->col_render.perp_wall_dist;
+	if (game->dda.side == 0)
+		game->col_render.wall_x = game->player.pos_y + d * game->dda.ray_dir_y;
+	else
+		game->col_render.wall_x = game->player.pos_x + d * game->dda.ray_dir_x;
 	game->col_render.wall_x -= floor(game->col_render.wall_x);
 }
 
 void	select_texture(t_game *game)
 {
-	if (game->dda.side == 0) {
-		game->col_render.texture = game->dda.ray_dir_x > 0 ? &game->map_data->east : &game->map_data->west;
-	} else {
-		game->col_render.texture = game->dda.ray_dir_y > 0 ? &game->map_data->south : &game->map_data->north;
+	if (game->dda.side == 0)
+	{
+		if (game->dda.ray_dir_x > 0)
+			game->col_render.texture = &game->map_data->east;
+		else
+			game->col_render.texture = &game->map_data->west;
+	}
+	else
+	{
+		if (game->dda.ray_dir_y > 0)
+			game->col_render.texture = &game->map_data->south;
+		else
+			game->col_render.texture = &game->map_data->north;
 	}
 }
 
 void	compute_texture_x_coord(t_game *game)
 {
-	game->col_render.texture_x = (int)(game->col_render.wall_x * game->col_render.texture->width);
-	if ((game->dda.side == 0 && game->dda.ray_dir_x > 0) || (game->dda.side == 1 && game->dda.ray_dir_y < 0))
-		game->col_render.texture_x = game->col_render.texture->width - game->col_render.texture_x - 1;	
+	int	t_width;
+
+	t_width = game->col_render.texture->width;
+	game->col_render.texture_x = (int)(game->col_render.wall_x * t_width);
+	if ((game->dda.side == 0 && game->dda.ray_dir_x > 0)
+		|| (game->dda.side == 1 && game->dda.ray_dir_y < 0))
+		game->col_render.texture_x = t_width - game->col_render.texture_x - 1;
 }
 
 void	draw_ceiling_and_floor(t_game *game, int x)
